7307ce93972484d1c4221851d24a643004192fb3
its1801f2211 <71606885+DulfyMans@users.noreply.github.com>
2022-11-29 15:37:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# Practice commands for Linux shell
+
+# Shell is a program that can be used to run commands
+# Usually called system commands
+
+#~/its-1801-f22-cli-01$ is called a prompt
+
+# ~ represents the home directory
+# / is a separator for directories and or files
+# its-1801-f22-cli-01 is another directory
+
+# ls is for listing files & directories in the current directory
+
+# ls - l is for long list format
+# The first letter indicates if it is a directory (d) or a file (-)
+
+# ls -al is for long format and showing hidden files
+
+# Hidden files have names that start with a dot (.)
+
+# cd is for changing directories (notice that the prompt also reflects the change)
+
+# To go up one directory:
+# cd ..
+
+# Shells usually have autocomplete using the tab key
+
+# pwd (present working directory) can show the full path to the directory you are currently in
+
+# cat command (catalog)
+# To show the contents of a text file (file that has characters that are printable on the screen) 
